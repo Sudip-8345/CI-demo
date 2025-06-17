9439e93ecdea2390ec5e9500394d9a78d5ab4d64
Sudip <sudipd2004@gmail.com>
2025-06-17 23:26:15 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a+b
+
+
+
+
+
+def subtract(a,b):
+    return a-b
+
+res1 = add(5, 3)
+res2 = subtract(10, 4)
+
+print(f"Addition Result: {res1}")
+print(f"Subtraction Result: {res2}")
